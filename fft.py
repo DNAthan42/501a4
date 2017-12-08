ee384bd5e7542d9c4c9da73376f1c917e05c8011
nathan.douglas <nathan.douglas@ucalgary.ca>
2017-12-08 12:14:47 -0700

    First draft of fft convolution

--- fft.py
+++ fft.py
@@ -47,18 +47,43 @@ def fft(inFile, irFile, outFile):
     waveFile.writeFile(outName, y)
 
 def convolve(x, h):
+    #load the c ver file
+    lib = ctypes.CDLL("liba6.so")
+
     #find necessary padding
-    minPad = 2* len(x)
+    biggerArg = max([len(x), len(h)])
     pad = 1
-    #double pad until it is larger than minpad
-    while pad < minPad:
+    #double pad until it is larger than the bigger arr
+    while pad < biggerArg:
         pad << 1
     
-    #create and fill the output buffer
-    y = []
-    for i in range(0, pad):
-        y.append(0.0)
+    #pad to double size for FFT
+    x_pad = []
+    h_pad = []
+    i = 0
+    while i < 2*pad:
+        x_pad[i] = x[i] if i < len(x) else 0.0
+        h_pad[i] = h[i] if i < len(h) else 0.0
 
-    #convert to ctypes
-    c_y = (ctypes.c_double * pad)(*y)
+    #convert to ctypes for fft
+    c_x_pad = (ctypes.c_double * 2 * pad)(*x)
+    c_h_pad = (ctypes.c_double * 2 * pad)(*h)
     
+    #run fft
+    lib.four1(c_x_pad, pad, 1)
+    lib.four1(c_h_pad, pad, 1)
+
+    #complex multiplication
+    y = []
+    for i in range(0, 2*pad, 2):
+        y[i] = (c_x_pad[i] * c_h_pad[i]) - (c_x_pad[i+1] * c_h_pad[i+1])
+        y[i+1] = (c_x_pad[i+1] * c_h_pad[i+1]) + (c_x_pad[i] * c_h_pad[i])
+
+    #convert to ctypes for ifft
+    c_y_pad = (ctypes.c_double * 2 * pad)(*y)
+
+    #run ifft
+    lib.four1(c_y_pad, pad, -1)
+
+    #truncate and return
+    return c_y_pad[:len(x) + len(h) - 1]
