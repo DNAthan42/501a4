e6de23ba178896a62b65aeff480152c6aaa941f4
nathan.douglas <nathan.douglas@ucalgary.ca>
2017-12-08 11:06:46 -0700

    Starting new file to do convolution using FFT

--- fft.py
+++ fft.py
@@ -2,3 +2,12 @@ import sys
 import wav_file
 import ctypes
 
+####MAIN
+if len(sys.argv) != 4:
+    sys.stderr.write("Usage: fft inputfile, IRfile, outputfile")
+    sys.stderr.flush()
+    sys.exit(-1)
+fft(sys.argv[1], sys.argv[2], sys.argv[3])
+
+def fft(inFile, irFile, outFile):
+    pass
